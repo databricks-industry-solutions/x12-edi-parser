a26928fe0320b844eb8ce297ee1dcbcffee7a830
Aaron Z <aaron.zavora@databricks.com>
2024-03-20 09:49:25 -0400

    adding functional tests

--- tests/test_edi.py
+++ tests/test_edi.py
@@ -6,6 +6,7 @@ class TestEDI(PysparkBaseTest):
 
     x = EDI(open("sampledata/837/CC_837I_EDI.txt", "rb").read().decode("utf-8"))
     y = EDI(open("sampledata/837/CC_837P_EDI.txt", "rb").read().decode("utf-8"))
+    z = EDI(open("sampledata/837/CHPW_Claimdata.txt", "rb").read().decode("utf-8"))
     #
     # Test counting total number of segments 
     # 
@@ -27,7 +28,14 @@ class TestEDI(PysparkBaseTest):
         assert(len(TestEDI.x.segments_by_position(0,100)) == 66)
         assert(TestEDI.x.segments_by_position(15, 20)[0].segment_name() == "NM1")
 
-    
-        
+
+    def test_functional_groups(self):
+        assert(len(TestEDI.z.functional_segments()) == 1)
+        assert(len(TestEDI.z.functional_segments()[0].data) == 173)
+        assert(TestEDI.z.num_transactions() == 5)
+        assert(TestEDI.z.num_functional_groups() == 1)
+        assert(TestEDI.z.functional_segments()[0].data[0].element(0) == "GS" and TestEDI.z.functional_segments()[0].data[172].element(0) == "GE")
+        assert(len([a.element(0) for a in TestEDI.z.functional_segments()[0].data if a.element(0) == "ST"]) == TestEDI.z.num_transactions())
+            
 if __name__ == '__main__':
     unittest.main()
