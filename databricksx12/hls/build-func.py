235f0f1abab0b687f133cc479bd7d155a8539568
Raven <raven.mukherjee@databricks.com>
2024-04-11 11:24:16 -0400

    two classes in two files; one for finding the hierarchical loops and the other for asking complex questions about claims. Also a new edited sample data example with dependent sub-child in Line 161

---
+++ databricksx12/hls/build-func.py
@@ -0,0 +1,138 @@
+"""
+Apr 9 notes
+"""
+from databricksx12.edi import *
+
+class LoopMapping:
+
+    def __init__(self, mappings=None):
+        self.mappings = (mappings if mappings is not None else {
+            '20': {
+                'description': 'Information Source',
+                'loop': '2000A'
+                },
+            '22': {
+                'description': 'Subscriber',
+                'loop': '2000B'
+                }
+            })
+
+
+   
+        
+    """
+    def __init__(self):
+        self.mappings = {
+            '2000A': ('20', 'NM1', '3'),
+            '2000B': ('22', 'SBR', '4'),
+        }
+
+
+    ADZ want our key = (lookup value found in data), value = additional info needed 
+    """
+
+
+class HierarchicalLoop(EDI):
+    def __init__(self, data, delim_cls=AnsiX12Delim, loop_mapping= LoopMapping.mappings):
+        super().__init__(data, delim_cls)
+        self.loop_mapping = loop_mapping
+        self.target_element, self.target_segment_name, self.target_element_index = self.loop_mapping.get_identifiers(
+            Loop)
+
+        # find all HL segments along with the 3rd element that denotes 2000A (20) or 2000B (22)
+        self.hl_segments = self._hl_identifiers()
+
+        # find all CLM segments (important for indexing the last HL or SBR within a tx)
+        self.clm_segments = self._clm_identifiers()
+
+        # Calculate ranges and then extract 2000A/B lines based on those ranges
+        self.ranges = self.select_range_of_interest(
+            self.hl_segments, self.clm_segments, self.target_element)
+        self.extracted_lines = self.extract_lines_based_on_ranges(
+            self.ranges, self.target_segment_name, self.target_element_index)
+
+    def _hl_identifiers(self):
+        # Find the segments where HL loop begins
+        indexed_HL_segments = self.segments_by_nloop_object.extracted_linesame_index("HL")
+        return [(i, x.element(3)) for i, x in indexed_HL_segments]
+
+    def _clm_identifiers(self):
+        # Find the segments where CLM loop begins
+        indexed_CLM_segments = self.segments_by_name_index("CLM")
+        return [(i, x.element(2)) for i, x in indexed_CLM_segments]
+
+    def select_range_of_interest(self, hl_indexes, clm_indexes, target_value):
+        ranges = []
+        start_index = None
+        last_index = None
+
+        for index, value in hl_indexes:
+            if value == target_value:
+                if start_index is not None:
+                    ranges.append((start_index+1, index))
+                start_index = index
+            elif start_index is not None:
+                ranges.append((start_index+1, index))
+                start_index = None
+        if clm_indexes:
+            last_index = clm_indexes[-1][0]
+        if last_index and start_index is not None:
+            ranges.append((start_index+1, last_index))
+        return ranges
+
+    def extract_lines_based_on_ranges(self, ranges, target_value, target_index):
+        extracted_elements = []
+        # Iterate through each range in the list
+        for start, end in ranges:
+            # Retrieve the segments within this range
+            segments_in_range = self.segments_by_position(start, end)
+
+            desired_elements = map(
+                lambda segment: segment.element(int(target_index)),
+                filter(
+                    lambda segment: segment.segment_name() == target_value and len(
+                        segment.data.split(segment.format_cls.ELEMENT_DELIM)) > int(target_index),
+                    segments_in_range
+                )
+            )
+            extracted_elements.extend(desired_elements)
+
+        return list(extracted_elements)
+
+
+    def parent_loops(self):
+        pass
+
+    def child_loops(self, parent_loop_num):
+        pass
+
+    """
+       @return
+         -index of each HL segment
+         -index of parent segments
+         -be able to answer "where is loop XYZ?" and "at this location, what looop am i in?"
+    """
+    def _hl_segment_indexes(self):
+        pass
+
+
+    self.hl_parents = {
+        parent:
+        { index_start : value
+          index_end : value
+           children : [
+               hl_child : {
+                   index_start: value
+                   index_end: value
+                   }
+               ]
+        }
+
+
+    self.hl = HL()
+    self.claim_start_index = segment(clm)
+
+    Who is my billing provider?  hl.get_loop(20)
+    Who is my subscriber?
+    Who is my patient? 
+    
--- databricksx12/hls/hierarchicalloop.py
+++ databricksx12/hls/hierarchicalloop.py
@@ -1,136 +1,96 @@
 from databricksx12.edi import *
 
-class LoopMapping:
-
-    def __init__(self, mappings=None):
-        self.mappings = (mappings if mappings is not None else {
-            '20': {
-                'description': 'Information Source',
-                'loop': '2000A'
-                },
-            '22': {
-                'description': 'Subscriber',
-                'loop': '2000B'
-                }
-            })
-
-
-   
-        
-    """
-    def __init__(self):
-        self.mappings = {
-            '2000A': ('20', 'NM1', '3'),
-            '2000B': ('22', 'SBR', '4'),
-        }
-
-
-    ADZ want our key = (lookup value found in data), value = additional info needed 
-    """
-
 
 class HierarchicalLoop(EDI):
-    def __init__(self, data, delim_cls=AnsiX12Delim, loop_mapping= LoopMapping.mappings):
+    def __init__(self, data, delim_cls=AnsiX12Delim):
         super().__init__(data, delim_cls)
-        self.loop_mapping = loop_mapping
-        self.target_element, self.target_segment_name, self.target_element_index = self.loop_mapping.get_identifiers(
-            Loop)
 
-        # find all HL segments along with the 3rd element that denotes 2000A (20) or 2000B (22)
-        self.hl_segments = self._hl_identifiers()
+        # find all HL and SE segments to find start and end of loops + CLM segments
+        self.indexed_HL_segments = self.segments_by_name_index("HL")
+        self.indexed_SE_segments = self.segments_by_name_index("SE")
 
-        # find all CLM segments (important for indexing the last HL or SBR within a tx)
-        self.clm_segments = self._clm_identifiers()
+        # parent and children loops
+        self.parent_loops = self._parent_loops()
+        self.child_loops = self._child_loops(self.parent_loops)
+        self.subchild_loops = self._child_loops(self.child_loops)
 
-        # Calculate ranges and then extract 2000A/B lines based on those ranges
-        self.ranges = self.select_range_of_interest(
-            self.hl_segments, self.clm_segments, self.target_element)
-        self.extracted_lines = self.extract_lines_based_on_ranges(
-            self.ranges, self.target_segment_name, self.target_element_index)
+    def _parent_loops(self):
+        parent_start_loops = []
+        for i, segment in self.indexed_HL_segments:
+            # Check if the second element is empty (and the third element is '20' and the last element is '1')
+            if segment.element(2) == '':
+                # index of parent, counter, and if child
+                parent_start_loops.append(
+                    (i, segment.element(1), segment.element(-1)))
 
-    def _hl_identifiers(self):
-        # Find the segments where HL loop begins
-        indexed_HL_segments = self.segments_by_nloop_object.extracted_linesame_index("HL")
-        return [(i, x.element(3)) for i, x in indexed_HL_segments]
+        parent_end_loops = [i for i, x in self.indexed_SE_segments]
+        return [(tup + (j,)) for tup, j in zip(parent_start_loops, parent_end_loops)]
 
-    def _clm_identifiers(self):
-        # Find the segments where CLM loop begins
-        indexed_CLM_segments = self.segments_by_name_index("CLM")
-        return [(i, x.element(2)) for i, x in indexed_CLM_segments]
+    def _child_loops(self, parent_loops):
+        child_loops = []
+        for parent_start_index, counter, child_id, parent_stop_index in parent_loops:
+            if child_id == '1':
+                for i, segment in self.indexed_HL_segments:
+                    if segment.element(2) == counter:
+                        # index of child, parent/tx counter, and if sub-child
+                        child_loops.append(
+                            (i, counter, segment.element(-1), parent_stop_index))
 
-    def select_range_of_interest(self, hl_indexes, clm_indexes, target_value):
-        ranges = []
-        start_index = None
-        last_index = None
-
-        for index, value in hl_indexes:
-            if value == target_value:
-                if start_index is not None:
-                    ranges.append((start_index+1, index))
-                start_index = index
-            elif start_index is not None:
-                ranges.append((start_index+1, index))
-                start_index = None
-        if clm_indexes:
-            last_index = clm_indexes[-1][0]
-        if last_index and start_index is not None:
-            ranges.append((start_index+1, last_index))
-        return ranges
-
-    def extract_lines_based_on_ranges(self, ranges, target_value, target_index):
-        extracted_elements = []
-        # Iterate through each range in the list
-        for start, end in ranges:
-            # Retrieve the segments within this range
-            segments_in_range = self.segments_by_position(start, end)
-
-            desired_elements = map(
-                lambda segment: segment.element(int(target_index)),
-                filter(
-                    lambda segment: segment.segment_name() == target_value and len(
-                        segment.data.split(segment.format_cls.ELEMENT_DELIM)) > int(target_index),
-                    segments_in_range
-                )
-            )
-            extracted_elements.extend(desired_elements)
-
-        return list(extracted_elements)
+        # If child_id is greater than 1, recursively call the fn
+            if int(child_id) > 1:
+                child_loops.extend(self._child_loops(
+                    [(parent_start_index, counter, str(int(child_id) - 1), parent_stop_index)]))
+        return child_loops
 
 
-    def parent_loops(self):
-        pass
+class HierarchicalLoopManager:
+    def __init__(self, data, delim_cls=AnsiX12Delim):
+        self.hl = HierarchicalLoop(data, delim_cls)
+        self.summary = {}
+        self.generate_summary()
 
-    def child_loops(self, parent_loop_num):
-        pass
+    def generate_summary(self):
+        for pl in self.hl.parent_loops:
+            parent_summary = {
+                'parent_index_start': pl[0],
+                'parent_index_end': pl[-1],
+                'children': []
+            }
+            # Find children loops for this parent tx
+            children = []
+            for cl in self.hl.child_loops:
+                if pl[0] < cl[0] < pl[-1]:
+                    children.append({
+                        'child_index_start': cl[0],
+                        'child_index_stop': cl[-1]
+                    })
 
-    """
-       @return
-         -index of each HL segment
-         -index of parent segments
-         -be able to answer "where is loop XYZ?" and "at this location, what looop am i in?"
-    """
-    def _hl_segment_indexes(self):
-        pass
+            # Add children to parent summary
+            parent_summary['children'] = children
 
-
-    self.hl_parents = {
-        parent:
-        { index_start : value
-          index_end : value
-           children : [
-               hl_child : {
-                   index_start: value
-                   index_end: value
-                   }
-               ]
-        }
+            # Add HL 1 counter to the summary as the key
+            self.summary[pl[1]] = parent_summary
 
 
-    self.hl = HL()
-    self.claim_start_index = segment(clm)
+"""
+loop_manager = HierarchicalLoopManager(sample_data_837i_edited)  
+summary = loop_manager.summary 
 
-    Who is my billing provider?  hl.get_loop(20)
-    Who is my subscriber?
-    Who is my patient? 
-    
-    
+output:
+{'1': {'parent_index_start': 7,
+  'parent_index_end': 35,
+  'children': [{'child_index_start': 16, 'child_index_stop': 35}]},
+ '63': {'parent_index_start': 41,
+  'parent_index_end': 69,
+  'children': [{'child_index_start': 50, 'child_index_stop': 69}]},
+ '49': {'parent_index_start': 75,
+  'parent_index_end': 103,
+  'children': [{'child_index_start': 84, 'child_index_stop': 103}]},
+ '75': {'parent_index_start': 109,
+  'parent_index_end': 138,
+  'children': [{'child_index_start': 118, 'child_index_stop': 138}]},
+ '79': {'parent_index_start': 144,
+  'parent_index_end': 179,
+  'children': [{'child_index_start': 153, 'child_index_stop': 179},
+   {'child_index_start': 160, 'child_index_stop': 179}]}}
+"""
