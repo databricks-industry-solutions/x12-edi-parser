b8d23d47ad2f335cd0a2d7dc7e3aa4a8e26d07fd
Raven <raven.mukherjee@databricks.com>
2024-05-02 01:49:14 -0400

    adjusted Loop and LoopManager to process nested loops with modified claim data with children

--- databricksx12/hls/hierarchicalloop.py
+++ databricksx12/hls/hierarchicalloop.py
@@ -1,40 +1,41 @@
 from databricksx12.edi import *
-import functools
 
+import itertools
 
 class HierarchicalLoop(EDI):
     def __init__(self, data, delim_cls=AnsiX12Delim):
         super().__init__(data, delim_cls)
-        self.parent_start_loops = self._parent_start_tup_loops()  # returns tuple; to check
+
+        # parent and children loops
+        self.parent_start_loops = self._parent_start_tup_loops()
         self.parent_end_loops = self._parent_end_loops()
         self.parent_loops = self._parent_loops()
         self.child_loops = self._child_loops(self.parent_loops)
-        self.subchild_loops = self._child_loops(self.child_loops) # recursive cases
-
+        self.subchild_loops = self._subchild_loops(self.child_loops)
+    
     def _parent_start_tup_loops(self):
-        # index of parent, counter, and if child
-        # TODO unit test to return tuple
-        return [(i, x.element(1), x.element(-1)) for i, x in self.segments_by_name_index("HL") if x.element(2) == ""]
+        # index of parent, counter, and if child 
+        return [(i, x.element(1), x.element(-1)) for i, x in self.segments_by_name_index("HL") if x.element(2) == ""] # TODO unit test to return tuple
 
     def _parent_end_loops(self):
         return [i for i, x in self.segments_by_name_index("SE")]
-
+    
     def _parent_loops(self):
         return [(tup + (j,)) for tup, j in zip(self.parent_start_loops, self.parent_end_loops)]
 
+
     def _child_loops(self, parent_loops):
         child_loops = [(i, counter, segment.element(-1), parent_stop_index)
-                       for _, counter, child_id, parent_stop_index in parent_loops if int(child_id) == 1
-                       for i, segment in self.segments_by_name_index("HL") if segment.element(2) == counter]
+                    for _, counter, child_id, parent_stop_index in parent_loops
+                    for i, segment in self.segments_by_name_index("HL") if segment.element(2) == counter]
+        return child_loops
 
-        # recursive cases where child_id is greater than 1 == sub_child
-        subchild_cases = filter(lambda x: int(x[2]) > 1, parent_loops)
-        subchild_loops = map(
-            lambda x: self._child_loops(
-                [(x[0], x[1], str(int(x[2]) - 1), x[3])]),
-            subchild_cases
-        )
-        return functools.reduce(lambda acc, lst: acc + lst, subchild_loops, child_loops)
+    def _subchild_loops(self, child_loops):
+        it1, it2 = itertools.tee(child_loops)
+        next(it2, None) 
+        return [pair[1] for pair in zip(it1, it2) if int(pair[0][2]) == 1]
+
+
 
 
 class HierarchicalLoopManager:
@@ -43,23 +44,52 @@ class HierarchicalLoopManager:
         self.summary = self.generate_summary()
 
     def get_child_loops(self, parent_loop, loops):
-        return [loop for loop in loops if parent_loop[0] < loop[0] < parent_loop[-1]]
+        """Filter child loops that fall within the given parent loop's range."""
+        return list(filter(lambda x: parent_loop[0] < x[0] < parent_loop[3], loops))
 
-    def process_loop(self, loop, level=0):
-        child_loops = self.get_child_loops(loop, self.hl.child_loops)
-        children = [self.process_loop(child, level + 1) for child in child_loops]
+    def calculate_child_end_index(self, current_child, next_child, parent_end):
+        """Calculate the end index of a child, adjusting to avoid overlap with the next child."""
+        return min(current_child[3], next_child[0] - 1 if next_child else parent_end)
+    
+    def process_child(self, child, subchildren, parent_end):
+        """Map a single child loop to its dictionary representation, including subchildren if applicable."""
+        return {
+            'index_start': child[0],
+            'index_end': self.calculate_child_end_index(child, None, parent_end),  # No next_child directly handled here
+            'children': subchildren if subchildren else None
+        }
+    
+    def add_subchildren_to_children(self, children, subchild_loops, parent_end):
+        """Map function to add subchildren to corresponding children, exclude children that are subchildren."""
+        #subchild_ids = {sc[1] for sc in subchild_loops}  # Set of parent_ids for subchildren
+        children = [child for child in children if child not in subchild_loops]  # Filter out subchildren
+        # subchild_lookup = {sc[1]: sc for sc in subchild_loops}  # Lookup for subchildren by parent counter
         
-        loop_summary = {
+        # Process each child, include subchildren when applicable
+        return list(map(lambda child: self.process_child(
+            child,
+            [{ 'index_start': subchild[0],
+               'index_end': self.calculate_child_end_index(subchild, None, child[-1]),
+               'children': None } 
+             for subchild in subchild_loops if subchild[1] == child[1] and int(child[2]) == 1],
+            parent_end),
+            children))
+
+    def process_loop(self, loop):
+        child_loops = sorted(self.get_child_loops(loop, self.hl.child_loops), key=lambda x: x[0])
+        children = self.add_subchildren_to_children(child_loops, self.hl.subchild_loops, loop[3])
+        return {
             'index_start': loop[0],
-            'index_end': loop[-1],
+            'index_end': loop[3],
             'children': children or None
         }
-        return loop_summary
 
     def generate_summary(self):
-        """Generate a hierarchical summary for each top-level parent loop."""
-        loop_processing = lambda loop: (str(loop[1]), self.process_loop(loop))
-        return dict(map(loop_processing, self.hl.parent_loops))
+        return {str(loop[1]): self.process_loop(loop) for loop in self.hl.parent_loops}
+
+
+
+
 
 
 """
@@ -80,10 +110,17 @@ output:
   'index_end': 138,
   'children': [{'index_start': 118, 'index_end': 138, 'children': None}]},
  '79': {'index_start': 144,
-  'index_end': 179,
-  'children': [{'index_start': 153,
-    'index_end': 179,
-    'children': [{'index_start': 160, 'index_end': 179, 'children': None}]},
-   {'index_start': 160, 'index_end': 179, 'children': None}]}} 
-   # the last 'children' list, there is a repeat that is tricky to remove
+  'index_end': 186,
+  'children': [{'index_start': 153, 'index_end': 186, 'children': None},
+   {'index_start': 160,
+    'index_end': 186,
+    'children': [{'index_start': 167, 'index_end': 186, 'children': None}]}]}}
+"""
+"""
+sample_data_837p = open("./sampledata/837/837p.txt", "rb").read().decode("utf-8").replace("\\n", "")
+HierarchicalLoopManager(sample_data_837p).summary
+{'1': {'index_start': 7,
+  'index_end': 42,
+  'children': [{'index_start': 12, 'index_end': 42, 'children': None},
+   {'index_start': 27, 'index_end': 42, 'children': None}]}}
 """
