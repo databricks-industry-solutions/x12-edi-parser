c67800567259578f4e1235280d998d919225d023
Aaron Z <aaron.zavora@databricks.com>
2024-10-02 14:07:00 -0400

    bump vesrion

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="databricksx12",
-    version="0.0.1",
+    version="0.0.2",
     # python_requires='>=3.9.*',
     python_requires='>=3.9',
     author="Aaron Zavora, Raven Mukherjee",
