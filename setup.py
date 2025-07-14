f28ff66f2765fc017d7b47ad6e0fe697aaab89e5
Aaron Z <aaron.zavora@databricks.com>
2025-07-14 14:46:44 -0400

    versions

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="databricksx12",
-    version="0.0.5",
+    version="0.0.6",
     # python_requires='>=3.9.*',
     python_requires='>=3.9',
     author="Aaron Zavora, Raven Mukherjee",
