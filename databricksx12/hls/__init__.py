ada2721b0e6287ae7b016157f3f9539caff68cce
Aaron Zavora <zavoraad@gmail.com>
2026-01-07 11:26:06 -0500

    avoiding infinite loops in loop.py if the file is malformed

--- databricksx12/hls/__init__.py
+++ databricksx12/hls/__init__.py
@@ -3,4 +3,10 @@ from .enrollment import *
 from .loop import *
 from .remittance import *
 from .healthcare import *
-from .mapinarrow_functions import *
+
+# Optional imports - only available if pyarrow/pyspark are installed
+try:
+    from .mapinarrow_functions import *
+except ImportError:
+    # pyarrow/pyspark not available - mapinarrow_functions will not be available
+    pass
