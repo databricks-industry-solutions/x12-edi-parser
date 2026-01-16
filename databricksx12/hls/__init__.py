5d833a27019e5880d534c8f7ef0d84cef9e46e8a
Aaron Zavora <zavoraad@gmail.com>
2026-01-15 20:14:56 -0500

    updating performance for 837s

--- databricksx12/hls/__init__.py
+++ databricksx12/hls/__init__.py
@@ -1,12 +1,11 @@
 from .claim import *
 from .enrollment import *
-from .loop import *
 from .remittance import *
 from .healthcare import *
 
 # Optional imports - only available if pyarrow/pyspark are installed
 try:
-    from .mapinarrow_functions import *
+	from .mapinarrow_functions import *
 except ImportError:
     # pyarrow/pyspark not available - mapinarrow_functions will not be available
     pass
